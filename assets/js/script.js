a0e2d416946048d20fce32a783d854c94facfee5
blkmnt <132701818+blkmnt@users.noreply.github.com>
2024-09-21 00:10:38 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card-container');
-    const today = new Date(); // Utilisation de la date actuelle sans formatage
+    const today = new Date();
 
     cards.forEach(card => {
-        const cardDate = new Date(card.getAttribute('data-date')); // Création d'un objet Date
+        const cardDate = new Date(card.getAttribute('data-date'));
 
         if (cardDate < today) {
-            // Si le jour est passé, afficher uniquement la face recto
             card.querySelector('.card-back').style.display = 'none';
         } else if (cardDate.toDateString() === today.toDateString()) {
-            // Si c'est le jour J, afficher la face verso et gérer le flip
             card.querySelector('.card-back').style.display = 'flex';
             card.querySelector('.card-back button').onclick = () => flipCard(card.querySelector('.card'));
         } else {
-            // Si c'est un jour futur, afficher uniquement la face verso avec bouton inactif
             card.querySelector('.card-back').style.display = 'flex';
             const button = card.querySelector('.card-back button');
             button.classList.add('inactive');
