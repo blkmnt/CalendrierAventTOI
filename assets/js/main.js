b258b686fbbc215a0237780ab0976e0830ae510f
blkmnt <132701818+blkmnt@users.noreply.github.com>
2024-09-20 23:09:23 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+// Sélectionner les cartes
+const cards = document.querySelectorAll('.card-container');
 
+// Parcourir les cartes
+cards.forEach(cardContainer => {
+    const button = cardContainer.querySelector('.btn-discover');
+    
+    // Activer l'effet de flip pour le jour J
+    if (button.classList.contains('active')) {
+        button.addEventListener('click', () => {
+            cardContainer.classList.toggle('flipped');
+        });
+    }
+});
